bb72473f777e8869769185d8e8554796e1cae92c
Terurinpic <37729191+Terurinpic@users.noreply.github.com>
2024-04-26 13:56:39 +0900

    created splash controller (#14)
    Co-authored-by: teru <highcool39@gmail.com>

--- lib/screens/splash/screen.dart
+++ lib/screens/splash/screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:share_data/screens/splash/controller.dart';
+
+class SplashScreen extends HookConsumerWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // controller
+    final splashController = ref.read(splashControllerProvider);
+
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        await Future.delayed(const Duration(seconds: 3));
+
+        splashController.init();
+      });
+
+      return null;
+    }, []);
+
     return const Scaffold(
       body: Center(
         child: Icon(Icons.image_outlined),
